9b65f5250c89c13c8a55c180ad959eb083e10607
Brittany <bkhartmire@gmail.com>
2019-07-29 01:39:56 +0900

    write specs

--- test/spec.js
+++ test/spec.js
@@ -2,8 +2,6 @@ const chai = require("chai");
 const expect = chai.expect;
 const url = "http://localhost:3000";
 const request = require("supertest")(url);
-const config = require("../config");
-const knex = require("knex")(config.db);
 
 describe("GraphQL Queries", () => {
   it("should return Restaurants", done => {
@@ -25,6 +23,103 @@ describe("GraphQL Queries", () => {
         expect(res.body.data.Restaurants[0]).to.deep.equal({
           id: 1,
           name: "Golden Formosa"
+        });
+        done();
+      });
+  });
+  it("should return cuisines", done => {
+    const queryBody = {
+      query: `query {
+                Cuisines(limit: 3) {
+                    id
+                    name
+                }
+            }`
+    };
+    request
+      .post("/graphql")
+      .send(queryBody)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.Cuisines.length).to.equal(3);
+        expect(res.body.data.Cuisines[0]).to.deep.equal({
+          id: 1,
+          name: "Taiwanese"
+        });
+        done();
+      });
+  });
+  it("should return countries", done => {
+    const queryBody = {
+      query: `query {
+                Countries(limit: 3, offset: 2) {
+                    id
+                    name
+                }
+            }`
+    };
+    request
+      .post("/graphql")
+      .send(queryBody)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.Countries.length).to.equal(3);
+        expect(res.body.data.Countries[0]).to.deep.equal({
+          id: 3,
+          name: "Macau"
+        });
+        done();
+      });
+  });
+  it("should return cities", done => {
+    const queryBody = {
+      query: `query {
+                Cities(limit: 2, offset: 5) {
+                    id
+                    name
+                }
+            }`
+    };
+    request
+      .post("/graphql")
+      .send(queryBody)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.Cities.length).to.equal(2);
+        expect(res.body.data.Cities[1]).to.deep.equal({
+          id: 7,
+          name: "Helsinki"
+        });
+        done();
+      });
+  });
+  it("should get country name of a restaurant", done => {
+    const queryBody = {
+      query: `query {
+                Restaurant(name: "Danny's Steakhouse") {
+                    name
+                    stars
+                    price
+                    city_name
+                    country_name
+                }
+            }`
+    };
+    request
+      .post("/graphql")
+      .send(queryBody)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.Restaurant).to.deep.equal({
+          name: "Danny's Steakhouse",
+          stars: 1,
+          price: "$$",
+          city_name: "Taipei",
+          country_name: "Taiwan"
         });
         done();
       });
