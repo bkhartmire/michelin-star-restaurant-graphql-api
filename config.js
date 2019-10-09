e4001674377434c4e2bcf114545f0c5785774e8a
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-09 09:42:33 -0700

    rename config according to heroku

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    db: {
-      client: "pg",
-      connection: process.env.DB_URL || {
-        host: process.env.DB_HOST || "127.0.0.1",
-        port: process.env.DB_PORT || 5432,
-        database: process.env.DB_NAME || "michelin",
-        user: process.env.DB_USER || "postgres",
-        password: process.env.DB_PASSWORD || "postgres",
-      },
-    },
-  
-    express: {
-      port: process.env.PORT || 3000,
-    },
-  
-    logger: {
-      format: "dddd MMMM Do YYYY, h:mm:ss a",
-    },
-  };
+  db: {
+    client: "pg",
+    connection: process.env.DATABASE_URL || {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME || "michelin",
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres"
+    }
+  },
+
+  express: {
+    port: process.env.PORT || 3000
+  },
+
+  logger: {
+    format: "dddd MMMM Do YYYY, h:mm:ss a"
+  }
+};
